d7f0b52ad8f880aa9d4fdb6f61634c28f2d86df0
GrenFORest <agurza23@yahoo.com>
2023-02-23 06:41:49 +0300

    day two

--- CW.js
+++ CW.js
@@ -2,4 +2,14 @@
 /*
 Be Concise IV - Index of an element in an array
 */
-const find = (array, element) => array.includes(element) ? array.indexOf(element): 'Not found';
+/*
+const find = (array, element) => 
+array.includes(element) ? array.indexOf(element): 'Not found';
+*/
+
+//23.02
+const stringToNumber = function(str){
+    return +str;
+}
+
+stringToNumber('1234');
--- FCC.js
+++ FCC.js
@@ -5,7 +5,14 @@
 asdfg
 */
 
+/*
 var myName;
 
 var a;
 a = 7;
+*/
+//23.02
+let a;
+a = 7;
+let b;
+b = a;
